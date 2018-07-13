19fd224308606f6c9be9ab322a49825c3ecc423f
jolson615 <jolson615@gmail.com>
2018-07-13 11:36:10 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# neighborhood-objects-ruby-oo
+# Neighborhood Objects
+
+We're going to practice defining classes and creating instances of those classes. Open up the neighborhood.rb file and let's get started.
---
+++ neighborhood.rb
@@ -0,0 +1,49 @@
+# Level 1:
+# Create a class called Student that has two attributes: a name, and a grade.
+
+
+
+# Now create instances of three different students using the .new method.
+
+
+
+# Confirm that the class works by printing out the first student's name
+
+
+
+# Level 2:
+# Create a class called School that has three attributes: a name, a type, and a size
+
+
+
+# Create instances of three individual schools using the .new method.
+
+
+
+# Confirm that the class works by printing out the name and size of the third school.
+
+
+
+# Level 3:
+# Create a class called House that has four attributes: an address, a size, a price, and a number of bedrooms.
+
+
+
+# Create instances of at least three individual houses using the .new method.
+
+
+
+# Confirm that the class works by printing out the name and size of the second house.
+
+
+
+# Level 4:
+# Put your three students in an array called my_students, your houses in an array for houses, and your schools in an array for schools.
+
+
+
+# Iterate over the student array, printing out "_____ is in grade __." For each of the students.
+
+
+
+# Iterate over the houses array and print out a description for each on. Do the same for your schools array.  
